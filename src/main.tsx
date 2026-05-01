38d16eead2ca02dc075d143e7ee8c4f153c9ddde
fornaxjp <fornaxjp@gmail.com>
2026-05-01 15:46:23 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+(window as any).onerror = (msg, url, line, col, error) => {
+    document.body.innerHTML = `<div style="color: red; padding: 20px; font-family: sans-serif;"><h1>Fatal Error</h1><p>${msg}</p><pre>${error?.stack || ''}</pre></div>`;
+};
+(window as any).onunhandledrejection = (event) => {
+    document.body.innerHTML = `<div style="color: red; padding: 20px; font-family: sans-serif;"><h1>Unhandled Promise Rejection</h1><p>${event.reason}</p></div>`;
+};
+console.log('Main.tsx loaded');
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
